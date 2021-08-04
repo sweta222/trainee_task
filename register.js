d5b9cde6e8c5e111354f52ebd2e29de83da05d5d
sweta222 <swetastuti2@gmail.com>
2021-08-04 13:35:55 +0530

    registration form using html,css,js done

---
+++ register.js
@@ -0,0 +1,10 @@
+const register = () => {
+    let firstname = document.getElementById("FN").value;
+    let lastname = document.getElementById("LN").value;
+    let gender = document.getElementById("G").value;
+    let mobile = document.getElementById("MN").value;
+    //alert("firstname is " + firstname, "lastname is" + lastname, "gender is" + gender, "mobile num is" + mobile);
+    alert(firstname + lastname + gender + mobile);
+}
+
+
